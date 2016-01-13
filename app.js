0f452cc02aefc8865a2e240a6b80516242559c46
Jessica Bell <[jbell@prepequity.com]>
2016-01-13 10:07:14 -0500

    more comments

--- app.js
+++ app.js
@@ -2,15 +2,15 @@
 (function(){
   var myApp = angular.module('myApp', [ ] );
 
+//are we working?
 console.log("app!");
 
-$(document).ready( function() {
+  $(document).ready( function() {
 
-  //Instantiate minicolors picker http://labs.abeautifulsite.net/jquery-minicolors/index.html#instantiation
-  $('.demo').minicolors();
+    //Instantiate minicolors picker http://labs.abeautifulsite.net/jquery-minicolors/index.html#instantiation
+    $('.demo').minicolors();
 
-
-}); //doc .ready
+  }); //doc .ready
 
  myApp.controller('ColorController' , function($scope) {
   //empty string tied to a style tag in the html to add the styles dictated by the picker
@@ -26,17 +26,16 @@ $(document).ready( function() {
       //sets a variable with the secondary font
       $scope.secondaryFont = $(".secondary-font").val();
           
-        //sets the body font style to the secondary font
-        $scope.newStyle = "body{font-family: " + $scope.secondaryFont+ "} ";
-        // adds new style to our style variable which is tied to the HTML so automatically updates
-        $scope.siteStyles += $scope.newStyle;
+      //sets the body font style to the secondary font
+      $scope.newStyle = "body{font-family: " + $scope.secondaryFont+ "} ";
+      // adds new style to our style variable which is tied to the HTML so automatically updates
+      $scope.siteStyles += $scope.newStyle;
 
-        //loops through each of the elements in the primary font elements array and sets those elements with the primary font 
-        jQuery.each($scope.primaryElements, function(i, val){
-          $scope.newStyle = val + "{font-family: " + $scope.primaryFont + "} ";
-          //updates the style tag
-          $scope.siteStyles += $scope.newStyle;
-          console.log($scope.siteStyles);
+      //loops through each of the elements in the primary font elements array and sets those elements with the primary font 
+      jQuery.each($scope.primaryElements, function(i, val){
+        $scope.newStyle = val + "{font-family: " + $scope.primaryFont + "} ";
+        //updates the style tag
+        $scope.siteStyles += $scope.newStyle;
       });//end .each
     });//end click
 
@@ -46,7 +45,7 @@ $(document).ready( function() {
         $scope.id = $(this).attr('id');
         $scope.newStyle = $scope.id + "{color: " + $scope.color + "} ";
         $scope.siteStyles += $scope.newStyle;
-     });//end focusout
+      });//end focusout
 
       //after the user takes focus off the background-color pickers, create style and add to new style tag
       $('.background-color-choice').focusout(function() {
@@ -55,7 +54,7 @@ $(document).ready( function() {
         $scope.id = $(this).attr('id');
         $scope.newStyle = "." + $scope.id +"{background-color: " + $scope.color + "} ";
         $scope.siteStyles += $scope.newStyle;
-     });//end focusout
+      });//end focusout
  
    
   });//end color controller
