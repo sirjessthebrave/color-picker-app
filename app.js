c46dec92ffa015f926f5d4ca8a8e422c49b23641
Jessica Bell <[jbell@prepequity.com]>
2016-01-25 16:25:17 -0500

    updating font picker

--- app.js
+++ app.js
@@ -34,13 +34,12 @@ console.log("app!");
  myApp.controller('ColorController' , function($scope) {
   //empty string tied to a style tag in the html to add the styles dictated by the picker
   $scope.siteStyles = "";
+	$scope.siteFonts = "";
   $scope.primaryElements = "";
   $scope.primaryFont = "";
   $scope.secondaryFont = "";
   $scope.newStyle =  "";
-     
-     
-     // add the variables as object key pairs then check to see if the element already exists in the object, then update key, then make new string to insert into head with new styles
+
 
     $('select').change(function(){
       console.log('change');
@@ -55,23 +54,22 @@ console.log("app!");
       $scope.secondaryFont = $(".secondary-font").val();
      // console.log('second font: '+ $scope.secondaryFont);   
       //sets the body font style to the secondary font
-      $scope.newStyle = "body{font-family: " + $scope.secondaryFont+ "} ";
+      $scope.newFont = "body{font-family: " + $scope.secondaryFont+ "} ";
       // adds new style to our style variable which is tied to the HTML so automatically updates
       //$scope.siteStyles += $scope.newStyle;
       //console.log('first site styles: ' + $scope.siteStyles);
-      
-    });//end submit
+    });//end change
+	 
+	 $('.set-fonts').click($scope.setFonts);
 
     //sets the new font styles for the page
     $scope.setFonts = function() {
-     // console.log('clicky!');
-      $scope.siteStyles +=  $scope.newStyle;
-
-        //loops through each of the elements in the primary font elements array and sets those elements with the primary font 
+      $scope.siteFonts = $scope.newFont;
+       //loops through each of the elements in the primary font elements array and sets those elements with the primary font 
       jQuery.each($scope.primaryElements, function(i, val){
-        $scope.newStyle = val + "{font-family: " + $scope.primaryFont + "} ";
+        $scope.newFont = val + "{font-family: " + $scope.primaryFont + "} ";
         //updates the style tag
-        $scope.siteStyles += $scope.newStyle;
+        $scope.siteFonts += $scope.newFont;
        // console.log('for each loop styles: ' + $scope.siteStyles);
       });//end .each
       //console.log('click ' +  $scope.siteStyles);
@@ -87,24 +85,12 @@ console.log("app!");
 					$scope.selector = $(this).attr('id');
 					$scope.cssAttr = $(this).attr("cssAttr");
 					
-					
 					if($scope.values != false && $scope.cssAttr != false ){
 						//call updateStyle function and pass through the attribute ID 
 						//(which html attribute you are targeting), the css attribute (what css you are setting), and the value of the color picker
 						updateStyle($scope.selector, $scope.cssAttr, $scope.values);
 					};
-				});
-        
-        
-      });//end focusout
-
-      //after the user takes focus off the background-color pickers, create style and add to new style tag
-      $('.background-color-choice').blur(function() {
-        //sett css attribute to whatever you are trying to set
-        $scope.cssAttr = 'background-color';
-        //call updateStyle function and pass through the attribute ID 
-        //(which html attribute you are targeting), the css attribute (what css you are setting), and the value of the color picker
-        updateStyle($(this).attr('id'), $scope.cssAttr, $(this).val());
+				}); 
       });//end focusout
 
       function updateStyle(selector, attribute, value){
